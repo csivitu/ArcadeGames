5c647720b467bde9c2bdbc4ca5fe766395bbfc27
Ajaiqmar <ajaqimar@gmail.com>
2022-10-06 14:33:27 +0530

    V1.0

---
+++ jsgames/simon/JS/index.js
@@ -0,0 +1,8 @@
+$("#rules-button").click(function(){
+  window.location.href = "https://en.wikipedia.org/wiki/Simon_(game)";
+});
+
+$("#play-button").click(function(){
+  $(".main-menu").css("display","none");
+  $(".board").css("display","block");
+});
